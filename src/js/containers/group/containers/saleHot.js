6656c48326ce2107b95e4f81595dc2d8b3687342
alanlanyang <summerchul@sina.cn>
2017-11-02 21:18:56 +0800

    yang

---
+++ src/js/containers/group/containers/saleHot.js
@@ -0,0 +1,15 @@
+import React from "react";
+class SaleHot extends React.Component{
+    constructor(props,context){
+        super(props,context)
+    }
+    render(){
+        return (
+                <div>
+                    热销哈哈哈
+                </div>
+            )
+    }
+}
+
+export default SaleHot
